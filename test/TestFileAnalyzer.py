25435e97d6b980ac7f260d50d3d0e8d1b2b7cac9
Pooshan Vyas <pooshan.vyas@gmail.com>
2019-02-10 17:29:27 -0800

    added more structure

--- test/TestFileAnalyzer.py
+++ test/TestFileAnalyzer.py
@@ -5,18 +5,17 @@ from src_files import fileAnalyzer
 
 class TestFileAnalyzer(unittest.TestCase):
     def test_positive_case(self):
-        result=fileAnalyzer.find_n_most_frequent_word('../test_few_words.txt',5)
+        result=fileAnalyzer.find_n_most_frequent_word('../src_files/test_few_words.txt',5)
         self.assertEqual(len(result), 4)
         self.assertEqual(result['hello'], 3)
 
     def test_negative_case(self):
         """
-
+        Test with empty input file
         :return:
         """
-        result=fileAnalyzer.find_n_most_frequent_word('../test_few_words.txt',5)
-        self.assertEqual(len(result), 4)
-        self.assertEqual(result['hello'], 3)
+        result=fileAnalyzer.find_n_most_frequent_word('../src_files/emptyfile.txt',5)
+        self.assertEqual(len(result), 0)
 
 if __name__ == '__main__':
     unittest.main()
